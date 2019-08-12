b1ae9bf8d072167337251087789dc64ac2588003
Madhuranga Sendheera <lilan.maduranga@gmail.com>
2019-08-12 11:11:27 +0530

    Added contact create page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddContactComponent } from './pages/add-contact/add-contact.component'
 import { ContactsComponent } from './pages/list-contacts/list.component'
 import { EditContactComponent } from './pages/edit-contact/edit.component'
 import { ViewContactComponent } from './pages/view-contact/view.component'
 
 const routes: Routes = [
   { path:"", redirectTo:'contacts', pathMatch:'full' },
+  { path: 'add', component:AddContactComponent},
   { path: 'contacts', component:ContactsComponent},
   { path: 'edit/:id', component:EditContactComponent},
   { path: 'view/:id', component:ViewContactComponent}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AppComponent } from './pages/main-page/app.component';
 import { ContactsComponent } from './pages/list-contacts/list.component'
 import { ViewContactComponent } from './pages/view-contact/view.component'
 import { EditContactComponent } from './pages/edit-contact/edit.component'
+import { AddContactComponent } from './pages/add-contact/add-contact.component'
 
 import { ContactsService } from './services/contacts.service'; 
 import { ContactNameComponent } from './components/contact-name/contact-name.component'
@@ -18,7 +19,8 @@ import { ContactNameComponent } from './components/contact-name/contact-name.com
     ContactsComponent,
     EditContactComponent,
     ViewContactComponent, 
-    ContactNameComponent
+    ContactNameComponent,
+    AddContactComponent
   ],
   imports: [
     BrowserModule,
